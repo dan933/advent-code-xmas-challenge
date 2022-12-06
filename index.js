e6ccdf55f1ddd21b94344f6bc782dcc5c8ab2b30
dan933 <dnadistrictservices@gmail.com>
2022-12-06 11:12:12 +1100

    day 4 part 1 complete

--- index.js
+++ index.js
@@ -1,79 +1,61 @@
+const data = require('./data.js')
 
-const rucksackData = require('./data.js');
+const regPair = /,|\n/
 
-//Each rucksack has two compartments
-//Every item type is identified by a single lowercase
-//or uppercase letter (that is, a and A refer to different types of items).
-
-//A ruck sack always has the same number of items in each compartment
-
-// so the first half of the characters represent 
-// items in the first compartment, while the second half of the characters
-// represent items in the second compartment.
-
-// Lowercase item types a-z have priorities 1 through 26
-// Uppercase item types A through Z have prioties 27 to 52
-
-// In the above example, the priority of the item type that appears in both compartments of each rucksack
-//  is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); 
-//  the sum of these is 157.
+//split each task group
+let cleanUpPairs = data.split(regPair);
 
 
-const regSplitLines = /\n/
+cleanUpTaskArray = [];
 
-//create an array of each rucksack
-const rucksackDataArr = rucksackData.split(regSplitLines);
+//itterate through the clean up pairs
+cleanUpPairs.forEach((cleanTask) => {
+    
+    const tasks = [];
 
-//elf group
-let elfGroup = []
+    const regFirstTask = /\d+/g
+    const taskArray =  cleanTask.match(regFirstTask)
+    const firstTask =taskArray[0];
+    const lastTask = taskArray[1];
+
+    for(let i = +firstTask; i <= +lastTask; i++){
+        tasks.push(i)
+    }
+
+    cleanUpTaskArray.push(tasks);
+
+})
 
 
-let specialTotal = 0;
+let fullyContainedCounter = 0;
 
-rucksackDataArr.forEach((rucksack, index) => {
+//itterate through clean up tasks
+cleanUpTaskArray.forEach((taskGroup, index) => {
+    let groupNumber = (index + 1)
 
-    //add rucksack to elfgroup
-    elfGroup.push(rucksack);
+    if( groupNumber % 2 == 0){
 
-    // If last member of group
-    if ( elfGroup.length % 3 === 0 ){
+        //get the group tasks
+        let firstTasks = cleanUpTaskArray[index - 1];
+        let secondTasks = taskGroup;
 
-        // //check which chars are the same between  between group 1 and 2
-        for(let i = 0; i < elfGroup[0].length; i++){
-            
-            //Find special key in all three groups
-            let charMatchGroupZeroOne = elfGroup[1].match(elfGroup[0][i]);
-            let charMatchGroupZeroTwo = elfGroup[2].match(elfGroup[0][i]);
-            
-            //if special key is found
-            if(charMatchGroupZeroOne && charMatchGroupZeroTwo){
+        //if first tasks is the longer array
+        if(firstTasks.length > secondTasks.length){
 
-                //item that is in all three rucksacks
-                let specialChar = elfGroup[0][i];
+            if( firstTasks[0] <= secondTasks[0] &&
+                firstTasks[firstTasks.length - 1] >= secondTasks[secondTasks.length - 1]){
 
-                const asciiValue = specialChar.charCodeAt()
-
-                //calculate letter to value
-                const rucksackValue = asciiValue >= 97 ? asciiValue - 96 : asciiValue - 38
-                specialTotal += rucksackValue;
-
-                //add value to total
-                
-
-                //break loop
-                break;
+                    fullyContainedCounter++
             }
-            
-        }
+        }else if ( secondTasks[0] <= firstTasks[0] && 
+                    secondTasks[secondTasks.length - 1] >= firstTasks[firstTasks.length - 1] ){
 
-        //reset elfgroup
-        elfGroup = [];
-
-
-
-
-
+                        fullyContainedCounter++
+                    }
+        
     }
 })
 
-console.log('specialTotal', specialTotal)
+
+// console.log('cleanUpTaskArray', cleanUpTaskArray)
+//console.log('fullyContainedCounter', fullyContainedCounter)
