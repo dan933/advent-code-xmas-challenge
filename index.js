65979dc8385d62c9cf57052669c54b68cf98b5e5
dan933 <dnadistrictservices@gmail.com>
2022-12-08 16:18:42 +1100

    Day 6 part 1 complete

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 const data = require('./data.js')
 
-const containers =  data.containers;
-const instructions = data.instructions;
+const stream = data.stream;
 
-const exampleContainers = data.exampleContainers;
-const exampleInstructions = data.exampleInstructions;
 
-//iterate over the instructions
-instructions.forEach((instruction) => {
+// create counter to keep track of the stream
+let counter = 0;
+
+//array for the marker
+let markerArray = [];
+
+//while loop to itterate over the stream
+while(markerArray.length < 4){
+
+    //current stream char
+    let currentStream = stream[counter]
+
+
+    //stream char is already in array
+    let IsInvalidMarker = markerArray.includes(currentStream)
+
+    if(IsInvalidMarker){
+
+        //finds the index of the repeated char
+        let invalidMarkerIndex = markerArray.indexOf(currentStream);
+
+        //resets the array after the invalid marker
+        markerArray = markerArray.slice(invalidMarkerIndex + 1);        
+    }
+
+    //if marker has signal found
+    if(markerArray.length === 4){
+        break;    
+    }
     
-    let itemsMoved;
+    //add to stream array
+    markerArray.push(currentStream);
 
-    let startIndex = 0;
+    counter++;
+    
 
-    let endIndex = instruction.move;
+}
 
-    //remove from container
-    itemsMoved = containers[instruction.from - 1].splice(startIndex, endIndex);
-    containers[instruction.to - 1] = itemsMoved.concat(containers[instruction.to - 1]);
-})
-
-let topCreateCode = '';
-
-containers.forEach((container) =>
-{
-        topCreateCode += container[0];
-});
-
-console.log('topCreateCode', topCreateCode)
+console.log(markerArray)
+console.log(counter)
